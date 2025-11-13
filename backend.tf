1f828a0219b54d8983d114c956982e705d8f31d9
Harsh Personal <dungaraniharsh03@gmail.com>
2025-11-13 12:49:50 +0530

    bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "dev-state-1x"
+    bucket         = "tf-state-ozxcs"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
