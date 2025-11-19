6e0f7ea084f1914a1a74fd2f97addaa7a780e346
Harsh Personal <dungaraniharsh03@gmail.com>
2025-11-19 17:08:01 +0530

    added outputs

---
+++ outputs.tf
@@ -0,0 +1,25 @@
+# VPC Outputs
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+
+# EC2 Outputs
+
+# Jump Server
+output "jump_server_public_ip" {
+  value = module.ec2.jump_server_public_ip
+}
+
+# Private Server
+output "private_server_ip" {
+  value = module.ec2.private_server_ip
+}
